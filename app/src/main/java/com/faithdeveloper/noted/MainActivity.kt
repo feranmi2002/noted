cd3328fe6f42403d952e8f8cd25ba2c78fba36ce
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-02-21 15:52:38 +0100

    Improved sign in, sign up and verification ui.

--- app/src/main/java/com/faithdeveloper/noted/MainActivity.kt
+++ app/src/main/java/com/faithdeveloper/noted/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.faithdeveloper.noted
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.faithdeveloper.noted.ui.fragments.OnboardingFragment
 import com.faithdeveloper.noted.ui.fragments.OnboardingFragmentDirections
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    private lateinit var database:FirebaseFirestore
+    private lateinit var database: FirebaseFirestore
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
@@ -21,13 +20,22 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
         checkIfUserExists(auth.currentUser)
     }
-    private fun checkIfUserExists(currentUser: FirebaseUser?) = if (currentUser != null && currentUser.isEmailVerified){
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        navController.navigate(OnboardingFragmentDirections.actionOnboardingFragmentToNotesFragment())
-    }else{
-//         do nothing
-    }
 
+    private fun checkIfUserExists(currentUser: FirebaseUser?) =
+        if (currentUser != null && currentUser.isEmailVerified) {
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+            navController = navHostFragment.navController
+            navController.navigate(OnboardingFragmentDirections.actionOnboardingFragmentToNotesFragment())
+        } else {
+//         do nothing
+        }
+
+    override fun onResume() {
+        auth.currentUser?.reload()
+        if (auth.currentUser != null && auth.currentUser!!.isEmailVerified) {
+            navController.navigate(OnboardingFragmentDirections.actionOnboardingFragmentToNotesFragment())
+        }
+        super.onResume()
+    }
 }
